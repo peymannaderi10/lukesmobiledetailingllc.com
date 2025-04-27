c6b809ba137b9d36e13bd6a3105394e37b412a76
Peyman Naderi <peymannaderi10>
2025-04-26 23:42:49 -0400

    remove sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white/95 backdrop-blur-sm shadow-md fixed top-0 left-0 right-0 z-50">
+    <header className="bg-white shadow-md">
       <nav className="container-custom flex items-center justify-between py-2">
         <div className="flex lg:flex-1">
           <Link href="/" className="flex items-center gap-2">
